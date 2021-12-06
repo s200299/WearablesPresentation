e32aaa0c2162e9505943327c09398bdeb82f6bfd
MarkMunirMuehlenberg <mark-munir.muehlenberg@deutschebahn.com>
2021-12-06 21:20:55 +0100

    deleted unused ColorConverter.java

--- mobile/src/main/java/de/dhbw/wearables_presentation/ColorConverter.java
+++
@@ -1,8 +0,0 @@
-package de.dhbw.wearables_presentation;
-
-public class ColorConverter {
-    public String intToHex(int intColor){
-        return String.format("#%06X", (0xFFFFFF & intColor));
-    }
-
-}
